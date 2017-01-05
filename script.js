1c1e49b11d65c78d4bf0944fb72d797c19a1f22b
Divyank Pahlazani <pahlaz.divyank@gmail.com>
2017-01-05 23:21:32 +0530

    added google, facebook and twitter signin

--- script.js
+++ script.js
@@ -4,8 +4,13 @@
 	const reg_btn = $('#reg-btn');
 	const login_btn = $('#login-btn');
 	const logout_btn = $('#logout-btn');
+	const google_signin_btn = $('#google-signin-btn');
+	const fb_signin_btn = $('#fb-signin-btn');
+	const twitter_signin_btn = $('#twitter-signin-btn');
 
 	var google_provider = new firebase.auth.GoogleAuthProvider();
+	var fb_provider = new firebase.auth.FacebookAuthProvider();
+	var twitter_provider = new firebase.auth.TwitterAuthProvider();
 
 
 	// Initialize Firebase
@@ -28,7 +33,6 @@
   	const promise = auth.createUserWithEmailAndPassword(email, pass);
   	promise.catch(e => $("#log").text(e.message));
   });
-
   // Login User Event
   login_btn.on('click', e => {
   	const email = $('#email').val();
@@ -38,14 +42,11 @@
   	const promise = auth.signInWithEmailAndPassword(email, pass);
  		promise.catch(e => $("#log").text(e.message));
   });
-
   // Logout User Event
   logout_btn.on('click', e => {
   	const promise = firebase.auth().signOut();
   	promise.catch($("#log").text("Something went wrong"));
-
   });
-
   firebase.auth().onAuthStateChanged(user => {
   	if(user){
   		// Test purpose
@@ -63,6 +64,22 @@
   });
 
 
+  // Google Signin
+  google_signin_btn.on('click', e => {
+  	firebase.auth().signInWithRedirect(google_provider);
+  });
+
+
+  // Facebook Signin
+  fb_signin_btn.on('click', e => {
+  	firebase.auth().signInWithRedirect(fb_provider);
+  });
+
+
+  // Twitter Signin
+  twitter_signin_btn.on('click', e => {
+  	firebase.auth().signInWithRedirect(twitter_provider);
+  });
 
 
 
