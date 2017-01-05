3d0df6ee9b874cefb8f757fe28708e3d814a6b8c
Divyank Pahlazani <pahlaz.divyank@gmail.com>
2017-01-06 00:08:59 +0530

    testing

--- script.js
+++ script.js
@@ -66,19 +66,69 @@
 
   // Google Signin
   google_signin_btn.on('click', e => {
-  	firebase.auth().signInWithRedirect(google_provider);
+  	firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a Google Access Token. You can use it to access the Google API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		  alert("You are successfully login using Google");
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
   });
 
 
   // Facebook Signin
   fb_signin_btn.on('click', e => {
-  	firebase.auth().signInWithRedirect(fb_provider);
+  	firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		  alert("You are successfully login using Facebook");
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
   });
 
 
   // Twitter Signin
   twitter_signin_btn.on('click', e => {
-  	firebase.auth().signInWithRedirect(twitter_provider);
+  	firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+		  // You can use these server side with your app's credentials to access the Twitter API.
+		  var token = result.credential.accessToken;
+		  var secret = result.credential.secret;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		  alert("You are successfully login using Twitter");
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
   });
 
 
